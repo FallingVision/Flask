a523ca016c829579cb665baeaee40ffcb909fbcf
BeomChang <fnzlvkdvkd@naver.com>
2021-09-23 21:30:11 +0900

    client application 요청 test

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ normalize = transforms.Compose(
 
 app = Flask(__name__)
 
+@app.route('/test', methods=['GET'])
+def hello():
+    print('hello!')
+    return 'hello'
 
 @app.route('/inference', methods=['POST'])
 def inference():
@@ -26,4 +30,4 @@ def inference():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=2431)
+    app.run(debug=True, host="127.0.0.1", port=2431)
